c0d2d3f419651527065aa1553f9d7b061651dfc4
pokaljainam24 <pokaljainam@gmail.com>
2025-03-03 16:33:29 +0530

    done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const db = require('./configs/database');
 const adminAuth = require("./middlewares/adminAuth");  // Import once
 const movieRouter = require('./routers/movieRouter');
 const bodyParser = require('body-parser');
-const movieModel = require('./models/movieModel');
+const movieModel = require('./models/movieModel');     
 const bcrypt = require('bcrypt');
 const multer = require('multer');
 const path = require('path');
@@ -14,8 +14,10 @@ const port = 8055;
 const app = express();
 db();
 
+
 // Set view engine
 app.set('view engine', 'ejs');
+
 
 // Middleware setup
 app.use(express.static('public'));
@@ -23,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 app.use("/movies", movieRouter); // Make sure this line is present      
 app.use(cookieParser());
+
 
 // Routes
 const signupRoute = require("./routers/signupRouter");
@@ -37,6 +40,7 @@ app.use(adminRoute);  // adminAuth should be inside `movieRouter.js`
 const userRoute = require("./routers/clientRouter");
 app.use(userRoute);
 
+
 // about page
 app.get("/about", (req, res) => {
     res.render("pages/about", { movies: [] }); // Ensure movies is defined
@@ -46,6 +50,7 @@ app.get("/about", (req, res) => {
 app.get('/single', (req, res) => {
     res.render('pages/single'); // Make sure 'single.ejs' exists inside 'views/pages/'
 });
+
 
 // Start server
 app.listen(port, () => {
